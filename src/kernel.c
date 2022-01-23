744e1f84e66cc714216cc65f82abc65700657d1b
omid reza keshtkar <omidnw@outlook.com>
2022-01-23 07:24:46 +0330

    update kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,28 +1,23 @@
-typedef unsigned long long size_t;
+#include "library/stdlibrary/stdio.h"
 
-typedef struct{
-	void* BaseAddress;
-	size_t BufferSize;
-	unsigned int HorizontalResolution;
-	unsigned int VerticalResolution;
-	unsigned int PixelsPerScanLine;
-} FrameBuffer;
+void myne(FrameBuffer* MainGOPBuffer, PSFone_Font* psfone_font){
 
-void myne(FrameBuffer* MainGOPBuffer){
+        // unsigned int y = 50;
+	// unsigned int BBP = 4; // Bytes per pixel;
+	// /*
+	// each pixel is four bytes wide because it has
+	// a red, green, blue and alpha channel.
+	// alpha channel is unused in this framebuffer but it can
+	// be useful for when you are blending colors.
+	// */
 
-        unsigned int y = 50;
-	unsigned int BBP = 4; // Bytes per pixel;
-	/*
-	each pixel is four bytes wide because it has
-	a red, green, blue and alpha channel.
-	alpha channel is unused in this framebuffer but it can
-	be useful for when you are blending colors.
-	*/
+	// for(unsigned int x = 0; x < MainGOPBuffer->VerticalResolution / 2 * BBP; x+=BBP)
+	// {
+	// 	*(unsigned int*)(x + (y * MainGOPBuffer->PixelsPerScanLine * BBP) + MainGOPBuffer->BaseAddress)= 0xffffffff; // this address settings set all of the channels to 1
+	// }
 
-	for(unsigned int x = 0; x < MainGOPBuffer->VerticalResolution / 2 * BBP; x+=BBP)
-	{
-		*(unsigned int*)(x + (y * MainGOPBuffer->PixelsPerScanLine * BBP) + MainGOPBuffer->BaseAddress)= 0xffffffff; // this address settings set all of the channels to 1
-	}
-
-        return 0 ;
+	CursorPosition.X = 0;
+	CursorPosition.Y = 0;
+	// for(int test = 0; test < 50; test++) // test overflow print line just uncomment this line and add tab before the Print function.
+	Print(MainGOPBuffer, psfone_font, 0xffffffff, "Hello, world!");
 }
