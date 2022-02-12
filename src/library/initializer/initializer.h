525f94e6009ea0f7dbd3b95a9c4e45fbf6d3d929
omid reza keshtkar <omidnw@outlook.com>
2022-02-12 20:53:13 +0330

    remove old file.

--- src/library/initializer/initializer.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADERINA_LIBRARY_INITIALIZE
-#define ADERINA_LIBRARY_INITIALIZE
-
-#include "structures.h"
-
-#endif
--- src/library/initializer/typedefinitializer.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADERINA_LIBRARY_INITIALIZE_TYPES
-#define ADERINA_LIBRARY_INITIALIZE_TYPES
-
-typedef unsigned long long size_t;
-typedef unsigned int uniinteger;
-typedef unsigned long unilong;
-
-#endif
