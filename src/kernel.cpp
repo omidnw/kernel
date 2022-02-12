2355d24a3567c76ff983b8176923693f054945a0
omid reza keshtkar <omidnw@outlook.com>
2022-02-12 16:54:36 +0330

    remove comment. (yep just comment.)

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -2,14 +2,14 @@
 
 extern "C" void myne(FrameBuffer* MainGOPBuffer, PSFone_Font* psfone_font){
 
-        // unsigned int y = 50;
+    // unsigned int y = 50;
 	// unsigned int BBP = 4; // Bytes per pixel;
-	// /*
+	// 
 	// each pixel is four bytes wide because it has
 	// a red, green, blue and alpha channel.
 	// alpha channel is unused in this framebuffer but it can
 	// be useful for when you are blending colors.
-	// */
+	// 
 
 	// for(unsigned int x = 0; x < MainGOPBuffer->VerticalResolution / 2 * BBP; x+=BBP)
 	// {
